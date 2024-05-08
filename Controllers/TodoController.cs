4156330498aa493ad0e63d91d111e66f4bf3e453
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-08 13:19:56 -0300

    feat: delete get/post

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -46,4 +46,19 @@ public class TodoController : Controller {
         _context.SaveChanges();
         return RedirectToAction(nameof(Index));
     }
+
+    public IActionResult Delete(int id)
+    {
+        ViewData["Title"] = "Delete task";
+        var todo = _context.Todos.Find(id);
+        return View(todo);
+    }
+
+    [HttpPost]
+    public IActionResult Delete(Todo todo)
+    {
+        _context.Todos.Remove(todo);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
 }
