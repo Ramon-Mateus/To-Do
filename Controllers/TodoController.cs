06ebafe40dd615b5d3330434e222a5deb5dcf045
Ramon de Oliveira Araújo Mateus <ramonmateus00@gmail.com>
2024-05-08 12:43:42 -0300

    feat: create get/post

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using To_Do.Context;
+using To_Do.Models;
 
 namespace To_Do.Controllers;
 
@@ -9,10 +10,24 @@ public class TodoController : Controller {
 
     public TodoController(TodoContext context) => _context = context;
 
-    public IActionResult Index() {
+    public IActionResult Index()
+    {
         ViewData["Title"] = "To Do List";
         var todos = _context.Todos.ToList();
         return View(todos);
     }
-    
+
+    public IActionResult Create()
+    {
+        ViewData["Title"] = "New Task";
+        return View("Form");
+    }
+
+    [HttpPost]
+    public IActionResult Create(Todo todo)
+    {
+        _context.Todos.Add(todo);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
 }
